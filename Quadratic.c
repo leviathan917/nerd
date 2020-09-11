7630724c70f9b06863daddde41a18bf4775f9ce9
leviathan917 <65465542+leviathan917@users.noreply.github.com>
2020-09-11 21:31:14 +0100

    Update and rename README.md to Quadratic.c

---
+++ Quadratic.c
@@ -0,0 +1,22 @@
+//A program to determine the nature of roots of a quadratic equation 
+#include<stdio.h>
+int main(){
+float a, b, c,discriminant;
+printf("Give values of variables a,b and c\n"};
+scanf("%f%f%f",$a, $b, $c);
+discriminant = b^2-4*a*c
+if (discriminant >= 0){
+print("The roots of the quadratic equation are %f", $discriminant);
+print("Hence real and distinct roots");}
+else if (discriminant == 0){
+print("The roots of the quadratic equation are %f", $discriminant);
+print("Hence real and equal roots");}
+else (discriminant <= 0){
+print("The roots of the quadratic equation are %f", $discriminant);
+print("Hence complex or imaginary  roots");}
+return 0;
+}
+
+
+
+
